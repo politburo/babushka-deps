8481fd4960f7e7c025244566cea3f29a0c2a5040
Tal Rotbart <redbeard@gmail.com>
2013-03-01 14:27:49 +1100

    ElasticSearch installation. Continuing.

--- elasticsearch/elasticsearch.rb
+++ elasticsearch/elasticsearch.rb
@@ -2,7 +2,7 @@ dep("elasticsearch-running", :version, :port, :cluster_name) do
   requires_when_unmet ("elasticsearch-installed").with(version: version, port: port, cluster_name: cluster_name)
 
   version.default("0.20.5")
-  port.default(9200)
+  port.default("9200")
   cluster_name.default(`hostname`)
 
   met? {
@@ -20,7 +20,7 @@ dep("elasticsearch-installed", :version, :port, :cluster_name) do
     ("elasticsearch-init-script")
 
   version.default("0.20.5")
-  port.default(9200)
+  port.default("9200")
   cluster_name.default(`hostname`)
 
   met? {
@@ -30,13 +30,30 @@ dep("elasticsearch-installed", :version, :port, :cluster_name) do
 end
 
 dep("elasticsearch-extracted", :version) do
+  requires_when_unmet Dep("elasticsearch-downloaded").with(version: version)
 
   def elasticsearch_home
-    '' / 'usr/local/elasticsearch'
+    '/usr/local/elasticsearch'.p
   end
 
   met? {
     elasticsearch_home.exists?
+  }
+
+end
+
+dep("elasticsearch-downloaded", :version) do
+
+  def elasticsearch_tar_gz
+    '/tmp/elasticsearch-#{version}.tar.gz'.p
+  end
+
+  met? {
+    elasticsearch_tar_gz.exists?
+  }
+
+  meet {
+    shell "cd /tmp && wget http://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-#{version}.tar.gz ; cd -"
   }
 end
 
