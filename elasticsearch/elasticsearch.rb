bf591c130da04476f30cd2f8ca6183d0adc15754
Tal Rotbart <redbeard@gmail.com>
2013-03-01 14:01:45 +1100

    ElasticSearch installation. Continuing.

--- elasticsearch/elasticsearch.rb
+++ elasticsearch/elasticsearch.rb
@@ -1,7 +1,7 @@
 dep("elasticsearch-running", :port, :cluster_name) do
   requires_if_unmet "elasticsearch-installed"
 
-  port.default(:9200)
+  port.default(9200)
   cluster_name.default(`hostname`)
 
   met? {
