80aadc5b2719b4e0c0dd4336cfdb30f8c33c168c
Tal Rotbart <redbeard@gmail.com>
2013-03-01 13:48:01 +1100

    ElasticSearch installation. Continuing.

--- elasticsearch/elasticsearch.rb
+++ elasticsearch/elasticsearch.rb
@@ -1,3 +1,14 @@
-dep "elasticsearch-running" do
-  
+dep "elasticsearch-running", :port, :cluster_name do
+  requires_if_unmet "elasticsearch-installed"
+
+  port.default(:9200)
+  cluster_name.default(`hostname`)
+
+  met? {
+    shell "curl http://localhost:#{port}"
+  }
+
+  meet {
+    sudo "/etc/init.d/elasticsearch start"
+  }
 end
