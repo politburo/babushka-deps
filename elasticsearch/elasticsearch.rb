8e6544d9a29e475d8294d855e516ae1d9d86ce37
Tal Rotbart <redbeard@gmail.com>
2013-03-01 14:14:36 +1100

    ElasticSearch installation. Continuing.

--- elasticsearch/elasticsearch.rb
+++ elasticsearch/elasticsearch.rb
@@ -1,6 +1,7 @@
-dep("elasticsearch-running", :port, :cluster_name) do
-  requires_if_unmet "elasticsearch-installed"
+dep("elasticsearch-running", :version, :port, :cluster_name) do
+  requires_when_unmet Dep("elasticsearch-installed").with(version: version, port: port, cluster_name: cluster_name)
 
+  version.default("0.20.5")
   port.default(9200)
   cluster_name.default(`hostname`)
 
@@ -10,5 +11,29 @@ dep("elasticsearch-running", :port, :cluster_name) do
 
   meet {
     sudo "/etc/init.d/elasticsearch start"
+  }
+end
+
+dep("elasticsearch-installed", :version, :port, :cluster_name) do
+  requires , Dep("elasticsearch-extracted").with(version: version), Dep("elasticsearch-configured").with(port: port, cluster_name: cluster_name), Dep("elasticsearch-init-script")
+
+  version.default("0.20.5")
+  port.default(9200)
+  cluster_name.default(`hostname`)
+
+  met? {
+    true
+  }
+
+end
+
+dep("elasticsearch-extracted", :version) do
+
+  def elasticsearch_home
+    path '/usr/local/elasticsearch'
+  end
+
+  met? {
+    elasticsearch_home.exists?
   }
 end
