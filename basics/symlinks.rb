d55fd510ea0c58f45a49ba48d900674711d6cca9
Tal Rotbart <redbeard@gmail.com>
2013-03-27 13:45:27 +1100

    Symlink creation dep

---
+++ basics/symlinks.rb
@@ -0,0 +1,25 @@
+dep('symlink', :symlink_to, :symlink_path, :create_as) do
+  requires 'matrix data dir', 'matrix expanded'.with(matrix_version: matrix_version)
+  
+  create_as.default!(ENV['USER'])
+
+  def symlink
+    symlink_path.p
+  end
+
+  def symlink_points_to_target?
+    symlink_target = symlink.realpath.to_s
+    log "Symlink at '#{symlink}' pointing to: '#{symlink_target}'"
+  
+    symlink_target.eql?( symlink_to.p.realpath.to_s )
+  end
+  
+  met? { 
+     symlink.exists? && symlink_points_to_target?
+  }
+  
+  meet {
+    shell "ln -sf #{symlink_to} #{symlink}", as: create_as
+    log_ok "Set symlink '#{symlink}' -> '#{symlink_to}'"
+  }
+end
