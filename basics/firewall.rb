ae4dae4cbd740c4c4558dc4c78df9a5780c391d4
Tal Rotbart <redbeard@gmail.com>
2013-03-28 12:15:31 +1100

    Prototype ufw firewall.

---
+++ basics/firewall.rb
@@ -0,0 +1,29 @@
+require 'set'
+
+dep('firewall', :action, :from, :to_port) do
+  action.default!(:allow_in)
+  from.default!(:anywhere)
+
+  def parse_ufw_output(output)
+    ufw_rules = Set.new
+    output.scan(/^\[(?<rule_num>[\s\d]+)\]\s+(?<to_port>\d+\/?\S+)\s+(?<action>\S+\s?\S+)\s+(?<from>.*)$/) do | m | 
+      md = Regexp.last_match
+      ufw_rules << { 
+        to_port: md[:to_port], 
+        action: md[:action].gsub(/\s/, '_').downcase.to_sym, 
+        from: md[:from].include?('Anywhere') ? :anywhere : md[:from] 
+      }
+    end
+
+    ufw_rules
+  end
+
+  met? {
+    parse_ufw_output( sudo('ufw status numbered') ).include?({ to_port: to_port, action: action, from: from })
+  }
+
+  meet {
+    cmd = ("ufw #{ { allow_in: 'allow', deny_in: 'deny' }[action]} #{ from == :anywhere ? '' : "from #{from}"} to any #{to_port}")
+    log cmd
+  }
+end
